02b5d62212d4b55e2d17e2c60aaa3d651946187b
RahmaYasser <rahma.y9079@gmail.com>
2022-03-13 14:17:44 +0200

    witing db connection before running web service

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,5 @@ FROM python:3
 ADD . /code                         
 WORKDIR /code                        
 RUN pip install -r requirements.txt  
-RUN python manage.py migrate
+WORKDIR /code/wait-for-it
+RUN ./wait-for-it.sh -t 60 db:5432 -- python /code/manage.py migrate
--- discussion_boad/settings.py
+++ discussion_boad/settings.py
@@ -77,10 +77,10 @@ WSGI_APPLICATION = 'discussion_boad.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('NAME'),
-        'USER': os.getenv('USER'),
-        'PASSWORD': os.getenv('PASSWORD'), 
-        'HOST': os.getenv('HOST'), 
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres', 
+        'HOST': 'db', 
         'PORT': '5432',
 
     }
